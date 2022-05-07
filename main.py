14b5d8ea9878961ffc4a6f497bf1f9794bf4503b
andre-bamba <andre_mitchelle_bamba@dlsu.edu.ph>
2022-05-07 08:26:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-/**
+"""
 For Thesis Completion ...
-Author: Andre Bamba
+Author: Andre' Bamba
 Version: 1
 Program: A water quality monitoring and control program using ESP32 IoT connection with RPI controller
 Description: ESP832 controller monitors water quality and controls actuation features such as irrigation and dosing control.
              Control and monitoring communicated with RPi through MQTT protocol for IoT automation project.
              Makes use of freeRTOS for task distribution using two cores (ESP32 is dual core), one for monitoring and other for control.
+
 For monitoring:
     Water Temperature: DS18B20    GPIO 5
     TDS/EC: Grove TDS Meter       GPIO 33
     Water Flow: YF-S201           GPIO 32
     Water level: HC SR04          GPIO 25 26        
     Water pH: SEN0169V2 pH meter  GPIO ?  
+
 For control:
     4 x peristaltic pumps         GPIO 27 14 12 13
     2 chnl relay and soln valve   GPIO 16 17 18
@@ -19,4 +21,15 @@ For control:
   - Creating separate functions for data acquisition and nutrient dosing
   - Nutrient dosing algorithm (for remote automation)                         
   - pH monitoring/control functions
-**/
+"""
+# Main Program -----------------------------------------------------------------------
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from gpiozero import tPin, ePin_1, h20Temp, tdsPin, flowPin, pump_a, pump_b, pump_c, pump_d, irrig_a, irrig_b, irrig_c
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+
+
